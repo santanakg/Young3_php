0ceedb30eae8a475b73c3770b643567fc4c52b5d
Elenaudoel <viniciussantanadasilva314@gmail.com>
2024-05-25 09:50:41 -0300

    Aula  8

--- Aula 8/sessao.php
+++ Aula 8/sessao.php
@@ -15,13 +15,21 @@ else
         $name_user = $_POST['user'];
         $pass_user = $_POST['password'];
 
-        $sql_code = "SELECT * users WHERE user = '$name_user' AND $password = 'pass_user'";
+        $sql_code = "SELECT * FROM users WHERE user = '$name_user' AND password = '$pass_user'";
 
         $sql_result = $mysqli->query($sql_code);
 
         $quantidade_linhas = $sql_result->num_rows;
 
-        var_dump($quantidade_linhas);
+        if ($quantidade_linhas >= 1)
+        {
+            $_SESSION['user'] = $name_user;
+            header('Location: painel.php');
+        }
+        else
+        {
+            echo ('Location: index.php?erro');
+        }
     }
 }
 ?>
